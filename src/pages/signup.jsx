bfdcb10e40ee4af74ec87ddc946b0a1fb5788602
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-19 17:10:54 +0200

    React <> signup page added

---
+++ src/pages/signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AuthContainer from './components/AuthContainer';
+
+
+const Signup = () => {
+
+  return (
+    <AuthContainer>
+      
+    </AuthContainer>
+  );
+};
+
+export default Signup;
