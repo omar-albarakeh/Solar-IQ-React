b185bcb1fbc30c167981844542d86c3bd66607f5
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-19 17:42:21 +0200

    React <> userpanel services updated

--- src/services/userpanelservice.jsx
+++ src/services/userpanelservice.jsx
@@ -2,3 +2,23 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:3001/auth';
 
+
+export const blockUser = async (userId, token) => {
+  try {
+    const response = await axios.post(
+      `${API_BASE_URL}/block/${userId}`,
+      {}, 
+      {
+        headers: {
+          Authorization: `Bearer ${token}`, 
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error blocking user:', error.response?.data?.message || error.message);
+    throw error;
+  }
+};
+
+
