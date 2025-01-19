267bdea1dc6deda73ec747a2d6b274e22f537336
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-19 17:11:45 +0200

    React <> signup page updated

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import AuthContainer from './components/AuthContainer';
+import AuthTitle from '../components/AuthTitle';
 
 
 const Signup = () => {
 
   return (
     <AuthContainer>
-      
+      <AuthTitle title="Signup" subtitle="Create your account" />
     </AuthContainer>
   );
 };
