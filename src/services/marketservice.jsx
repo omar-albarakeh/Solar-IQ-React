6506827d313a553b8517ca0a6ffbc938c81c0b7f
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-19 20:16:45 +0200

    React <> market service added

---
+++ src/services/marketservice.jsx
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import '../styles/styles.css';
+const apiUrl = 'http://localhost:3001/items';
+
+export const getItems = async (page = 1, limit = 10) => {
+  return axios.get(`${apiUrl}?page=${page}&limit=${limit}`);
+};
+
