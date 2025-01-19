6e319ead59b9f53e605a720f7b2cd013d0f76003
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-19 16:47:14 +0200

    React <> auth title added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AuthContainer from '../components/AuthContainer';
-
+import AuthTitle from '../components/AuthTitle';
+import InputField from '../components/InputField';
+import AuthButton from '../components/authbutton';
+import AuthFooter from '../components/authfooter';
+import ErrorMessage from '../components/errormessage';
 
 const Login = () => {
+
   return (
     <AuthContainer>
-     
     </AuthContainer>
   );
 };
