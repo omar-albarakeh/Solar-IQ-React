3cf7f8b399dd70f0555e51d4eeb717d7cfd405c3
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-19 22:25:02 +0200

    React <> update form  page added

---
+++ src/pages/market/UpdateItemForm.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { getItemById, updateItem } from "../../services/marketservice";
+
+const UpdateItemForm = () => {
+
+
+  return (
+    <div className="update-item-form-container">
+      
+    </div>
+  );
+};
+
+export default UpdateItemForm;
