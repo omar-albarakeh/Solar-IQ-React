ff2ffaebdebed5ea601f3d2dadeebbefd8c18605
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-19 22:25:14 +0200

    React <> update form  page updated

--- src/pages/market/UpdateItemForm.jsx
+++ src/pages/market/UpdateItemForm.jsx
@@ -3,7 +3,19 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { getItemById, updateItem } from "../../services/marketservice";
 
 const UpdateItemForm = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [item, setItem] = useState({
+    name: '',
+    price: 0,
+    capacity: 0,
+    category: '',
+    description: '',
+    imageUrl: '',
+    quantity: 0,
+  });
 
+  
 
   return (
     <div className="update-item-form-container">
