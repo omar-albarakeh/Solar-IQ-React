f8ccf4102d9c102f0293c5a19fe05c3af8ec5cdd
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-19 16:35:27 +0200

    React <> loginpage added

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AuthContainer from './AuthContainer';
+
+
+const Login = () => {
+  return (
+    <AuthContainer>
+     
+    </AuthContainer>
+  );
+};
+
+export default Login;
