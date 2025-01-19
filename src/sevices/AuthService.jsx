91e9c04b6666cdc38f214cbd910b374cc634fb6b
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-19 16:53:26 +0200

    React <> auth services updated

--- src/sevices/AuthService.jsx
+++ src/sevices/AuthService.jsx
@@ -6,6 +6,18 @@ class AuthService {
       baseURL: 'http://localhost:3001/auth',
     });
   }
+
+
+  async login(email, password) {
+    try {
+      const response = await this.api.post('/login', { email, password });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  
 }
 
 export default new AuthService();
