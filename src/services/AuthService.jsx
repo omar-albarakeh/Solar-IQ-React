fa8a0ab0488abec465d1e345c4ddd20581e54824
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-19 17:09:44 +0200

    React <> auth service updated

--- src/services/AuthService.jsx
+++ src/services/AuthService.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 class AuthService {
   constructor() {
     this.api = axios.create({
-      baseURL: 'http://localhost:3001/auth',
+      baseURL: 'http://localhost:3001/auth', 
     });
   }
 
@@ -11,6 +11,15 @@ class AuthService {
   async login(email, password) {
     try {
       const response = await this.api.post('/login', { email, password });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async signup(userData) {
+    try {
+      const response = await this.api.post('/signup', userData);
       return response.data;
     } catch (error) {
       throw error;
