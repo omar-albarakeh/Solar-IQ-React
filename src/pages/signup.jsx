921b7eb97102fbcd7f11c5bf1a34f16c265838dd
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-19 17:12:09 +0200

    React <> signup page updated

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import AuthContainer from './components/AuthContainer';
 import AuthTitle from '../components/AuthTitle';
-
+import ErrorMessage from '../components/ErrorMessage';
 
 const Signup = () => {
 
   return (
     <AuthContainer>
       <AuthTitle title="Signup" subtitle="Create your account" />
+      <ErrorMessage message={error} />
     </AuthContainer>
   );
 };
