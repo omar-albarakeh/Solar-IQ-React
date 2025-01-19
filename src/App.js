d1161551df1865e58caf1cbd56fff9720c4915ef
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-19 17:27:08 +0200

    React <> admin page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
       </div>
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {jwtDecode} from 'jwt-decode';
+
+const Dashboard = () => {
+  
+    return (
+      <div className="dashboard-container">
+      </div>
+    );
+}
+
+export default Dashboard;
